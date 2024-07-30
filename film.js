a08f4f8cf994f7d7a04015b859d08381dfe8796b
Radit <rputraiskandar@gmail.com>
2024-07-30 19:51:06 +0700

    Add files via upload

---
+++ film.js
@@ -0,0 +1,56 @@
+function Mybutton() {
+    document.getElementById("hidden").style.display = "block";
+  }
+
+  function Mybtn() {
+    document.getElementById("hdn").style.display = "block";
+  }
+
+  function Mydblbtn() {
+    document.getElementById("hdn").style.display = "none";
+  }
+
+function Link1()
+{
+  window.location = "Heading.html"    
+}
+
+function Link2()
+{
+  window.location = "Paragraf.html"    
+}
+
+function Link3()
+{
+  window.location = "Format-Teks.html"    
+}
+
+function Link4()
+{
+  window.location = "Hyperlink.html"    
+}
+
+function Link5()
+{
+  window.location = "List.html"    
+}
+
+function Link6()
+{
+  window.location = "Style.html"    
+}
+
+function Link7()
+{
+  window.location = "Image.html"    
+}
+
+function Link8()
+{
+  window.location = "Tabel.html"
+}
+
+function Link9()
+{
+  window.location = "Video.html"    
+}
